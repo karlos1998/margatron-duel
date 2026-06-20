675b7dfd8c61c15255ec7d1a034b6a657bf69999
karlos1998 <fadeusz98@gmail.com>
2026-06-20 16:58:44 +0200

    fix: allow forcing https asset upgrades

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        @if (config('app.force_https'))
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        @endif
+
         @vite('resources/js/app.ts')
         @inertiaHead
     </head>
